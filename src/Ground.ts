a9719559145412a44e87c13dc88395879f582d05
huangchengjie <597857074@qq.com>
2023-08-22 23:45:18 +0800

    Init picture frame

--- src/Ground.ts
+++ src/Ground.ts
@@ -1,10 +1,33 @@
 import * as BABYLON from 'babylonjs'
+import pictureUrl from '../public/savanna.jpg'
 
 export class Ground {
   mesh: BABYLON.Mesh
 
   constructor(scene: BABYLON.Scene) {
-    this.mesh = BABYLON.MeshBuilder.CreateGround('Ground', { width: 10, height: 10 }, scene)
+    this.mesh = this.createMesh(scene)
+  }
+
+  private createMesh(scene: BABYLON.Scene) {
+    const ground = BABYLON.MeshBuilder.CreateGround('Ground', { width: 10, height: 10 }, scene)
+    const pictureFrame = BABYLON.MeshBuilder.CreateTube('PictureFrame', {
+      path: [
+        new BABYLON.Vector3(5.0, 0.0, -5.0),
+        new BABYLON.Vector3(-5.0, 0.0, -5.0),
+        new BABYLON.Vector3(-5.0, 0.0, 5.0),
+        new BABYLON.Vector3(5.0, 0.0, 5.0),
+        new BABYLON.Vector3(5.0, 0.0, -5.0),
+      ],
+      radius: 0.2,
+    })
+    pictureFrame.parent = ground
+    const pictureMtl = new BABYLON.StandardMaterial('PictureMtl')
+    const pictureTexture = new BABYLON.Texture(pictureUrl, scene, {})
+    pictureMtl.diffuseTexture = pictureTexture
+    ground.material = pictureMtl
+    ground.scaling.x = 540 / 360
+
+    return ground
   }
 
   static create(scene: BABYLON.Scene) {
